45054fadf0e011102303d3ee959929793022cfc8
clopezferrer <lopezferrer@gmail.com>
2022-07-09 12:47:47 -0400

    animations

--- js/app.js
+++ js/app.js
@@ -1,74 +1,52 @@
+//DEFINE ALL ELEMENTS IN THE DOM
 //buttons
 const comenzar = document.querySelector("#picture");
 const feedBtn = document.querySelector("#feed");
 const walkBtn = document.querySelector("#walk");
 const cureBtn = document.querySelector("#cure");
 const trainBtn = document.querySelector("#train");
-const sleepBtn = document.querySelector("#sleep");
 const playBtn = document.querySelector("#play");
 
 //indicators
-const hungerIndicator = document.querySelector("#hunger-indicator");
-const bladderIndicator  = document.querySelector("#bladder-indicator");
-const boredomIndicator  = document.querySelector("#boredom-indicator");
-const healthIndicator  = document.querySelector("#health-indicator");
-const skillIndicator  = document.querySelector("#skill-indicator");
+const hungerIndicator = document.querySelector("#hunger");
+const bladderIndicator  = document.querySelector("#bladder");
+const boredomIndicator  = document.querySelector("#boredom");
+const healthIndicator  = document.querySelector("#health");
+const skillIndicator  = document.querySelector("#skill");
 
 //game settings
 let petName = ""
-const maxHunger = 300;
-const maxBladder = 300;
-const maxBoredom = 300;
-const maxHealth = 300;
-const maxSkill = 300;
-
-let day = 20;
 
 //functions
 function startFunction () {
   if(petName === ""){
-    petName = prompt('Name your pet', 'Name');
-    document.getElementById("picture").src="images/puppy1.png";
-    const petNameTag = document.createElement("h4");
-    petNameTag.setAttribute('id','petnameh4');
-    petNameTag.innerText = petName;
-    petNameTag.style['font-size'] = 60 + "px";
-    petNameTag.style['line-height'] = 0 + "px";
-    petNameTag.style['position'] = "relative";
-    petNameTag.style['top'] = -100 + "px";
-    petNameTag.style['top'] = -100 + "px";
-    petNameTag.style['font-family'] = 'Lakki Reddy', cursive;
-    petNameTag.style['text-transform'] = "uppercase";
-    document.querySelector("#image").appendChild(petNameTag)
+    petName = prompt('How will you name your pet?', 'Your pet\'s name');
+    if (petName === "Your pet\'s name" || petName === "" || petName === null){
+      alert('You must name your pet to start');
+      petName = "";
+    }else{
+      document.getElementById("picture").src="images/puppy1.png";
+      const petNameTag = document.createElement("h4");
+      petNameTag.setAttribute('id','petnameh4');
+      document.querySelector("#image").appendChild(petNameTag);
+      petNameTag.innerText = petName;
+      petNameTag.style['font-size'] = 60 + "px";
+      petNameTag.style['line-height'] = 0 + "px";
+      petNameTag.style['position'] = "relative";
+      petNameTag.style['top'] = -100 + "px";
+      petNameTag.style['top'] = -100 + "px";
+      petNameTag.style['font-family'] = "Lakki Reddy";
+      petNameTag.style['cursor'] = "auto";
+      picture.style['cursor'] = "auto";
+      picture.style['animation'] = "dogMoving 8s";
+      picture.style['animation-iteration-count'] = "infinite"
+      hungerIndicator.innerText = 10;
+      bladderIndicator.innerText = 0;
+      boredomIndicator.innerText = 0;
+      healthIndicator.innerText = 10;
+      skillIndicator.innerText = 0;
+    }
   }
 }
+
 comenzar.addEventListener("click", startFunction);
-
-
-/*
-function miTamagotchi() {
-  this.hunger = maxHunger;
-  this.bladder = maxBladder;
-  this.boredom = maxBoredom;
-  this.health = maxHealth
-  this.skill = maxSkill;
-}
-
-
-let tmgch = new Tamagotchi();
-let sleepHpCount;
-let hungerHpCount;
-let playHpCount;
-let score = 0;
-core();
-let coreUpdate = setInterval(core, 100 * day);
-
-
-//Main function of tamagotchi
-function core() {
-  //console.log(tmgch);
-  hungerIndicator  = (tmgch.hunger / maxHunger * 100).toFixed(2);
-  sleepIndicator = (tmgch.sleep / maxSleep * 100).toFixed(2);
-  playIndicator = (tmgch.play / maxPlay * 100).toFixed(2);
-
-*/
